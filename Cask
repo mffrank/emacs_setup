71cc5e60b47b5066cef86bd79b4015d9d86c0088
mffrank <max.florian.frank@gmail.com>
2018-03-20 11:44:06 -0400

    Setup the basic emacs config without cask

--- Cask
+++
@@ -1,6 +0,0 @@
-(source melpa)
-(depends-on "pallet")
-(depends-on "evil")
-(depends-on "evil-surround")
-(depends-on "solarized-theme")
-
--- init.el
+++ init.el
@@ -1,10 +1,1 @@
-;; Using cask and pallet for package management
-;; Installation instructions for cask and pellet are here:
-;; https://github.com/rdallasgray/pallet
-
-(require 'cask "~/.cask/cask.el")
-(cask-initialize)
-(require 'pallet)
-;;(pallet-mode t)
-
 (org-babel-load-file "~/.emacs.d/configuration.org")
